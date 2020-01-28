d3ded1663a9f92cd795fd9b7fd97409c0d2d7abe
sanyath007 <sanyath007@gmail.com>
2020-01-28 23:02:07 +0700

    Added logout function and colored fortawesome icons.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -167,13 +166,13 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">          
           <li class="nav-item">
             <router-link to="/dashboard" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-tachometer-alt blue"></i>
               <p>Dashboard</p>
             </router-link>
           </li>
           <li class="nav-item">
             <router-link to="profile" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-user orange"></i>
               <p>Profile</p>
             </router-link>
           </li>
@@ -188,7 +187,7 @@
           </li> -->
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cog"></i>
+              <i class="nav-icon fas fa-cog green"></i>
               <p>
                 Management
                 <i class="right fas fa-angle-left"></i>
@@ -290,10 +289,16 @@
             </ul>
           </li> -->
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-power-off"></i>
-              <p>Logout</p>
+            <a class="nav-link" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+              <i class="nav-icon fas fa-power-off red"></i>
+              <p>{{ __('Logout') }}</p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
           </li>
         </ul>
       </nav>
